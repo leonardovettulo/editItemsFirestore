a508073c91260772ec8f22577f84dfcd9ddca9ff
Leonardo Vettulo <leonardovettulo@gmail.com>
2019-09-21 11:11:08 -0300

    Add items and delete with dblclick

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,23 @@ import { AngularFirestoreModule } from "angularfire2/firestore";
 import { ItemsComponent } from "./components/items/items.component";
 
 import { ItemService } from "./services/item.service";
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { AddItemComponent } from "./components/add-item/add-item.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent, ItemsComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    ItemsComponent,
+    NavbarComponent,
+    AddItemComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase, "ItemEditor"),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [ItemService],
   bootstrap: [AppComponent]
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -15,24 +15,34 @@ import { map } from "rxjs/operators";
 export class ItemService {
   itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
+  itemDoc: AngularFirestoreDocument<Item>;
 
   constructor(public afs: AngularFirestore) {
+    this.itemsCollection = this.afs.collection("items", ref =>
+      ref.orderBy("title", "asc")
+    );
     //this.items = this.afs.collection('items').valueChanges();
-    this.items = this.afs
-      .collection("items")
-      .snapshotChanges()
-      .pipe(
-        map(changes => {
-          return changes.map(a => {
-            const data = a.payload.doc.data() as Item;
-            data.id = a.payload.doc.id;
-            return data;
-          });
-        })
-      );
+    this.items = this.itemsCollection.snapshotChanges().pipe(
+      map(changes => {
+        return changes.map(a => {
+          const data = a.payload.doc.data() as Item;
+          data.id = a.payload.doc.id;
+          return data;
+        });
+      })
+    );
   }
 
   getItems() {
     return this.items;
+  }
+
+  addItem(item: Item) {
+    this.itemsCollection.add(item);
+  }
+
+  deleteItem(item: Item) {
+    this.itemDoc = this.afs.doc(`items/${item.id}`);
+    this.itemDoc.delete();
   }
 }
